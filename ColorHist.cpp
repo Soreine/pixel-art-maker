605e3e2c9ed9cd29b216b1733c6f27d82f473972
Soreine <soreine.plume@gmail.com>
2014-02-17 15:59:40 +0100

    sage:q

--- ColorHist.cpp
+++ ColorHist.cpp
@@ -1,17 +1,14 @@
 #include "ColorHist.h"
 
-#include "CImg.h"
+#include "CImg-1.5.7/CImg.h"
 using namespace cimg_library;
 
 ColorHist::ColorHist() {}
-
-ColorHist::ColorHist(char* file_image) {
-}
 
 int ColorHist::getColor(Color c) const {
   return this->rgbSpace[c.getR()][c.getG()][c.getB()];
 }
 
-void ColorHist::addColor(Color c) const {
+void ColorHist::addColor(Color c) {
   this->rgbSpace[c.getR()][c.getG()][c.getB()]++;
 }
--- ColorHist.h
+++ ColorHist.h
@@ -1,18 +1,21 @@
 #ifndef COLORHIST_H
 #define COLORHIST_H
 
+#include "Color.h"
+
+#define COLOR_RANGE 256
+
 class ColorHist {
  public :
   ColorHist();
 
-  ColorHist(char* file_image);
 
   int getColor(Color c) const;
 
   void addColor(Color c);
 
  private :
-  int[][][] rgbSpace;
+  int rgbSpace[COLOR_RANGE][COLOR_RANGE][COLOR_RANGE];
 };
 
 #endif
